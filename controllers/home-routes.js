cbae90c59f296688d272bec3ba884376ef86d88f
Your Name <you@example.com>
2022-10-14 11:25:25 -0500

    building views and get routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,43 +13,78 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-//GET all saved homes
+//GET all saved homes and decor
 router.get('/', async (req, res) => {
     try {
         const dbHomeData = await Home.findAll({
-           
+
             include: [
                 { model: User },
                 { model: Decor },
-              ],
+            ],
+        });
+
+        const dbDecorData = await Decor.findAll({
+
+            include: [
+                { model: User },
+                { model: Home },
+            ],
         });
 
         const homes = dbHomeData.map((home) => home.get({ plain: true }));
-
-        res.render('homepage', { homes, loggedIn: req.session.loggedIn,});
+        const decors = dbDecorData.map((decor) => decor.get({ plain: true }));
+        res.render('homepage', { homes, decors, loggedIn: req.session.loggedIn, });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-//GET the dashboard
+//GET the homes and decor to the dashboard 
 router.get('/dashboard', async (req, res) => {
     try {
         const dbHomeData = await Home.findAll({
-           
+
             include: [
                 { model: User },
                 { model: Decor },
-              ],
+            ],
+        });
+
+        const dbDecorData = await Decor.findAll({
+
+            include: [
+                { model: User },
+                { model: Home },
+            ],
         });
 
         const homes = dbHomeData.map((home) => home.get({ plain: true }));
-
-        res.render('dashboard', { homes, loggedIn: req.session.loggedIn,});
+        const decors = dbDecorData.map((decor) => decor.get({ plain: true }));
+        res.render('dashboard', { homes, decors, loggedIn: req.session.loggedIn, });
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+//GET the decor to the dashboard 
+// router.get('/dashboard', async (req, res) => {
+//     try {
+//         const dbDecorData = await Decor.findAll({
+
+//             include: [
+//                 { model: User },
+//                 { model: Home },
+//             ],
+//         });
+
+//         const decors = dbDecorData.map((decor) => decor.get({ plain: true }));
+
+//         res.render('dashboard', { decors, loggedIn: req.session.loggedIn, });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 //Post Routes
 
