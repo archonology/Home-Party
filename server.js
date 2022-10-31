0dcb00dbfd498e116da14d9372bde158d9ce81c4
Reed Meher <reed.meher@gmail.com>
2022-10-30 22:00:05 -0500

    auto logout for idle time added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 60 * 1000},
   resave: false,
   saveUninitialized: true,
   // expiration: 60 * 60 * 1000,
